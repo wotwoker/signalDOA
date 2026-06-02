9d587ec3f0f206927611bd7ff1fc922a4379a4f4
wotwoker <jiangxaxut@qq.com>
2026-06-02 22:56:24 +0800

    add find_top_peaks

--- signaldoa/__init__.py
+++ signaldoa/__init__.py
@@ -1,11 +1,13 @@
 """Signal DOA estimation utilities."""
 
 from .cbf import CBFResult, cbf_spectrum, estimate_doa_cbf, steering_matrix, steering_vector
+from .peak_search import find_top_peaks
 
 __all__ = [
     "CBFResult",
     "cbf_spectrum",
     "estimate_doa_cbf",
+    "find_top_peaks",
     "steering_matrix",
     "steering_vector",
 ]
